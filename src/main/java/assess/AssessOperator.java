42a7c3b50b9c9d07b882bc3dcf17f69d20d3a96c
Spike Cifer <stelioszapp+dev@gmail.com>
2023-05-28 22:11:20 +0300

    Added Performance Results to Reporter

--- src/main/java/assess/AssessOperator.java
+++ src/main/java/assess/AssessOperator.java
@@ -11,10 +11,11 @@ import result.Cell;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Optional;
 
 /**
  * The top layer class for any assessments done in the intentional model.
@@ -23,10 +24,18 @@ import java.util.Optional;
  */
 public class AssessOperator {
     private final CubeManager cubeManager;
+    private final PerformanceResults performanceResults;
 
+    static class PerformanceResults {
+        long parseTime;
+        long comparisonTime;
+        long labelingTime;
+    }
     public AssessOperator(CubeManager cubeManager) {
         this.cubeManager = cubeManager;
+        this.performanceResults = new PerformanceResults();
     }
+
 
     /* Executes the provided query. Returns ?
      * @param assessQuery The user-provided query for assessment reasons
@@ -34,13 +43,24 @@ public class AssessOperator {
      * @throws RecognitionException If the query does not follow the defined syntax
      */
     public List<LabeledCell> execute(String assessQuery) throws RecognitionException {
+        Instant start = Instant.now();
         AssessQuery parsedQuery = parseQuery(assessQuery);
+        Instant end = Instant.now();
+        performanceResults.parseTime = Duration.between(start, end).toMillis();
+
+        start = Instant.now();
         HashMap<Cell, Double> comparisonResults = executeComparison(parsedQuery);
+        end = Instant.now();
+        performanceResults.comparisonTime = Duration.between(start, end).toMillis();
+
+        start = Instant.now();
         List<LabeledCell> results = labelResults(parsedQuery, comparisonResults);
+        end = Instant.now();
+        performanceResults.labelingTime = Duration.between(start, end).toNanos();
+
         exportToMD(assessQuery, parsedQuery.outputName, results);
         return results;
     }
-
 
     private AssessQuery parseQuery(String assessQuery) throws RecognitionException {
         AssessQueryParser parser = createParser(assessQuery);
@@ -55,7 +75,6 @@ public class AssessOperator {
             CommonTokenStream tokens = new CommonTokenStream(lexer);
             return new AssessQueryParser(tokens);
         } catch (IOException e) {
-            // No idea what could go wrong here.
             throw new RuntimeException("There was an error while creating the Assess Query parser");
         }
     }
@@ -76,15 +95,17 @@ public class AssessOperator {
     }
 
     private void exportToMD(String assessQuery, String outputName, List<LabeledCell> results) {
-        try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter("OutputFiles/assessments/" + outputName + ".md"));
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("OutputFiles/assessments/" + outputName + ".md"));){
             writer.append("## Query\n");
             writer.append(assessQuery);
             writer.append("\n## Results");
             for (LabeledCell cell : results) {
                 writer.append(cell.toString());
             }
-            writer.close();
+            writer.append("\n## Performance Results\n");
+            writer.append("Parsing time: ").append(String.valueOf(performanceResults.parseTime)).append(" ms\n");
+            writer.append("Comparison time: ").append(String.valueOf(performanceResults.comparisonTime)).append(" ms\n");
+            writer.append("Labeling time: ").append(String.valueOf(performanceResults.labelingTime)).append(" ns\n");
         } catch (IOException ioe) {
             ioe.printStackTrace();
             System.out.println("Failed to export to MarkDown");
