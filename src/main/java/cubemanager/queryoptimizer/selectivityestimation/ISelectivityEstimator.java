6ea83aef5890f243a6f4d1c7675c01ecd95a800a
StavropoulosPanagiotis <156346473+StavropoulosPanagiotis@users.noreply.github.com>
2026-05-13 17:13:24 +0300

    change signature to accept factTableSize

--- src/main/java/cubemanager/queryoptimizer/selectivityestimation/ISelectivityEstimator.java
+++ src/main/java/cubemanager/queryoptimizer/selectivityestimation/ISelectivityEstimator.java
@@ -6,5 +6,5 @@ import cubemanager.cubebase.CubeQuery;
 
 public interface ISelectivityEstimator {
 
-	List<SelectivityResult> estimate(CubeQuery query);
+	List<SelectivityResult> estimate(CubeQuery query, int factTableSize);
 }
