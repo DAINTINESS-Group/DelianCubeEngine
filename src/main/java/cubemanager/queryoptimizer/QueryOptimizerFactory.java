43203c3ffbf1bd27d154c828fc69b3dced7c8b8c
StavropoulosPanagiotis <156346473+StavropoulosPanagiotis@users.noreply.github.com>
2026-05-23 21:57:11 +0300

    pass input folder, cube name for histogram/sample creation

--- src/main/java/cubemanager/queryoptimizer/QueryOptimizerFactory.java
+++ src/main/java/cubemanager/queryoptimizer/QueryOptimizerFactory.java
@@ -4,10 +4,10 @@ import cubemanager.cubebase.CubeBase;
 
 public class QueryOptimizerFactory {
 
-    public static IQueryOptimization create(String operationType, CubeBase cubebase, String method, double sampleSize) {
+    public static IQueryOptimization create(String operationType, CubeBase cubebase, String method, String inputFolder, String cubeName) {
         switch (operationType.toUpperCase()) {
             case "SELECTIVITY_ESTIMATION" :
-                return new SelectivityEstimationOptimizer(cubebase, method, sampleSize);
+                return new SelectivityEstimationOptimizer(cubebase, method, inputFolder, cubeName);
             default:
                 throw new  IllegalArgumentException("Unknown operation type: " + operationType);
         }
