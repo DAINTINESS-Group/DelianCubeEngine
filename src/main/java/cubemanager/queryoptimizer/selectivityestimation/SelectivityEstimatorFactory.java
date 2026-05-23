b2cbeb61c9b50d837dcaa38128fab96ef6735874
StavropoulosPanagiotis <156346473+StavropoulosPanagiotis@users.noreply.github.com>
2026-05-23 22:06:26 +0300

    update parameters + combined

--- src/main/java/cubemanager/queryoptimizer/selectivityestimation/SelectivityEstimatorFactory.java
+++ src/main/java/cubemanager/queryoptimizer/selectivityestimation/SelectivityEstimatorFactory.java
@@ -4,14 +4,16 @@ import cubemanager.cubebase.CubeBase;
 
 public class SelectivityEstimatorFactory {
 
-	public static ISelectivityEstimator create(String method, CubeBase cubeBase, double sampleSize) {
+	public static ISelectivityEstimator create(String method, CubeBase cubeBase, String inputFolder, String cubeName) {
 		switch (method.toUpperCase()) {
 			case "FULL_TABLE_SCAN":
 				return new FullTableScanEstimator(cubeBase);
 			case "SAMPLING":
-				return new ReservoirSamplingEstimator(cubeBase, sampleSize);
+				return new ReservoirSamplingEstimator(inputFolder, cubeName, cubeBase);
 			case "HISTOGRAM":
-				return new HistogramEstimator(cubeBase);
+				return new HistogramEstimator(inputFolder, cubeName);
+			case "COMBINED":
+				return new CombinedEstimator(inputFolder, cubeName, cubeBase);
 			default:
 				throw new IllegalArgumentException("Unknown estimation method: " + method);
 		}
