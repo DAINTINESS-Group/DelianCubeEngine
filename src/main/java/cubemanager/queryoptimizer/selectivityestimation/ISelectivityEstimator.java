c0f96d70b90213c891305a59a6db4ca0dc77d201
StavropoulosPanagiotis <156346473+StavropoulosPanagiotis@users.noreply.github.com>
2026-08-29 15:53:21 +0300

    better javadoc

--- src/main/java/cubemanager/queryoptimizer/selectivityestimation/ISelectivityEstimator.java
+++ src/main/java/cubemanager/queryoptimizer/selectivityestimation/ISelectivityEstimator.java
@@ -6,6 +6,13 @@ import cubemanager.cubebase.CubeQuery;
 
 public interface ISelectivityEstimator {
 
+	/**
+	 * Estimates the selectivity of every sigma predicate in the query
+	 * @param query
+	 * @param factTableSize the number of rows in the fact table, used as the denominator
+	 * @return one result per sigma that could be resolved (the list could be shorter than the query's sigma list,
+	 * since unresolved predicates are skipped
+	 */
 	List<SelectivityResult> estimate(CubeQuery query, int factTableSize);
 
 	/**
